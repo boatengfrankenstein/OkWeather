5abf6538ce3b97f571e63e92505817240f595227
Daniel Boakye <countrybus1@gmail.com>
2019-10-06 09:23:31 -0400

    Update ShakeDetector.java

--- app/src/main/java/com/boakye/daniel/okweather/weatherui/ShakeDetector.java
+++ app/src/main/java/com/boakye/daniel/okweather/weatherui/ShakeDetector.java
@@ -1,5 +1,5 @@
 package com.boakye.daniel.okweather.weatherui;/*
- * Copyright 2014 Thomas Bouron
+
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
